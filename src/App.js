bfe35e769cc6954f20a920eefcdd6b176faa71f0
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2023-10-31 20:38:00 +0100

    ✨ F01: LOGIN > Add title and form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 //import logo from "../public/Logobarbenoire.jpg"
-import { useState } from 'react';
 import './App.css';
 import LoginPage from './components/pages/LoginPage.jsx';
 
 function App() {
-  const [userFirstName, setUserFirstName] = useState("Bob");
-  const [userLastName, setUserLastName] = useState("Dupond");
-  const [userMail, setUserMail] = useState("dupondbob@gmail.com");
 
   return (
     <LoginPage />
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+    //state
+    const [userFirstName, setUserFirstName] = useState("")
+
+    //comportements
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert("Bonjour")
+        setUserFirstName("")
+    }
+
+    const handleChange = (event) => {
+      setUserFirstName(event.target.value)
+    }
+
+
+    //affichage
   return (
     <div>
-      <form>
-        <h1>Bienvenue chez BARBE NOIRE !</h1>
-        <div className='sign-in'>
+    <h1>Bienvenue chez BARBE NOIRE !</h1>
+      <form action='submit'onSubmit={handleSubmit} >
           <h2>Connectez-vous:</h2>
-          <input type="text" placeholder='Entre votre prénom...' required />
-          <button type='submit'>Accédez à votre espace</button>
-        </div>
-        <div className='sign-up'>
-          <h2>Creer un compte:</h2>
-          <input type="text" placeholder='Entre votre nom...' />
-          <input type="text" placeholder='Entre votre prénom...' />
-          <input type="text" placeholder='Entre votre email...' />
-          <input type="text" placeholder='Entre un mot de passe...' />
-          <input type="text" placeholder='Confirmer mot de passe' />
-          <button type='submit'>Comfirmer</button>
-        </div>
+          <input value={userFirstName} onChange={handleChange} type="text" placeholder='Entre votre prénom...' required />
+          <button>Accédez à votre espace</button>
       </form>
         <div>
           <img src="logo" alt="logo" />
