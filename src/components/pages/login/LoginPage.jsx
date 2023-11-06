b6ff4e8302f9918d31bc52d27a1ff61694519b3c
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2023-11-06 13:37:11 +0100

    💄 F02: LOGIN > Add styling

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -14,10 +14,24 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background-color: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ::before {
+    content: '';
+    background: url("/images/BGBW2barbenoire.png");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
